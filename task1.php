868a620d3f142b42e31a8ade6f094ca880c1a6b2
naveenpranesh <naveenpranesh28@gmail.com>
2020-11-08 20:41:05 +0530

    string and array function assessment

---
+++ task1.php
@@ -0,0 +1,86 @@
+<?php
+error_reporting(0);
+if (isset($_POST['submit'])) {
+    $input = htmlspecialchars($_POST['input']);
+
+    $needtoreplace = htmlspecialchars($_POST['needtoreplace']);
+    $replace_word = htmlspecialchars($_POST['replace_word']);
+    $string_position = htmlspecialchars($_POST['string_position']);
+
+    $len = strlen($input);
+    $shuffled = str_shuffle($input);
+    $wordcount = str_word_count($input);
+    $replaced = str_replace($needtoreplace, $replace_word, $input);
+    $position = strrpos($replaced, $string_position);
+    $lower = strtolower($input);
+    $upper = strtoupper($input);
+
+    $first_letter = $input[0];
+    $last_letter = $input[$len - 1];
+
+    $reversed = strrev($input);
+
+    $f_upper = ucwords($input);
+
+    $f_lower = strtolower($input[0]) . substr($input, 1, $len - 1);
+    $md5 = md5($input);
+    $whitespaceremoved = trim($input);
+    $stringcompare = strcmp($input, $input);
+
+}
+
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+
+
+
+        <label>Input String:</label>
+        <input type="text" name="input" autocomplete="off">
+        <br>
+         <br>
+
+        <label>String need to be replaced:</label>
+        <input type="text" name="needtoreplace" autocomplete="off">
+        <br>
+         <br>
+
+        <label>Replacing String:</label>
+        <input type="text" name="replace_word" autocomplete="off">
+        <br>
+         <br>
+
+        <label>Input String to find its position:</label>
+        <input type="text" name="string_position" autocomplete="off">
+        <br>
+         <br>
+        <input type="submit" name="submit" autocomplete="off">
+
+        <h2>Output:</h2>
+        <label><h4>Length of the String is: <?php echo $len; ?></h4></label>
+        <label><h4>Shuffled String is: <?php echo $shuffled; ?></h4></label>
+        <label><h4>Word Count of the input is: <?php echo $wordcount; ?></h4></label>
+        <label><h4>Replaced String is: <?php echo $replaced; ?></h4></label>
+        <label><h4>Given string position is: <?php echo $position; ?></h4></label>
+        <label><h4>Lower Case of the String is: <?php echo $lower; ?></h4></label>
+        <label><h4>Upper Case of the String is: <?php echo $upper; ?></h4></label>
+        <label><h4>First Letter of the String is: <?php echo $first_letter; ?></h4></label>
+        <label><h4>Last Letter of the String is: <?php echo $last_letter; ?></h4></label>
+        <label><h4>Reversed String is: <?php echo $reversed; ?></h4></label>
+        <label><h4>First Letter Upper Case String is: <?php echo $f_upper; ?></h4></label>
+        <label><h4>First Letter Lower Case String is: <?php echo $f_lower; ?></h4></label>
+        <label><h4>md5 converted String is: <?php echo $md5; ?></h4></label>
+        <label><h4>White Space Removed String is: <?php echo $whitespaceremoved; ?></h4></label>
+        <label><h4>String Comparission: <?php echo $stringcompare; ?></h4></label>
+
+
+    </form>
+</body>
+
+</html>
---
+++ task3.php
@@ -0,0 +1,200 @@
+<?php
+error_reporting(0);
+//  static $arrays = array();
+if (isset($_POST['submit'])) {
+    $array_length = htmlspecialchars($_POST['array_length']);
+
+} else if (isset($_POST['proceed'])) {
+    $arrays = array();
+    $length = $_POST['length'];
+
+    for ($j = 0; $j < $_POST['length']; $j++) {
+        // array_push($arrays, $_POST[$j]);
+        $arrays[$_POST['index' . $j]] = $_POST[$j];
+    }
+    $created_array = $arrays;
+    if (!empty($_POST['insert'])) {
+        $inserted_array = $arrays;
+        array_push($inserted_array, $_POST['insert']);
+    }
+    if (!empty($_POST['delete'])) {
+        array_pop($arrays);
+        $deleted_array = $arrays;
+    }
+    if (!empty($_POST['num_delete'])) {
+        $num_deleted_array = array_splice($arrays, 0, $length - 3);
+
+    }
+    if (!empty($_POST['search'])) {
+        $search = array_search($_POST['search'], $created_array);
+
+    }
+
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+
+        <label>Array Length:</label>
+        <input type="number" name="array_length">
+        <br>
+
+        <input type="submit" name="submit" value="SUBMIT">
+        <br>
+        <br>
+
+        <label>Entered Array Length Value is:</label>
+        <input type="number" name="length" value='<?php echo $array_length; ?>' readonly>
+        <br>
+        <br>
+
+        <?php
+if (isset($_POST['submit'])) {
+    for ($i = 0; $i < $array_length; $i++) {?>
+
+        <label>Enter the array index <?php echo $i; ?>: </label>
+        <input type="text" name='<?php echo 'index' . $i; ?>' autocomplete="off">
+        <label>Enter the value of array index <?php echo $i; ?>: </label>
+        <input type="number" name='<?php echo $i; ?>' autocomplete="off"><br>
+        <?php }}?>
+        <br>
+        <br>
+
+
+        <label>Element to insert:</label>
+        <input type="number" name='insert' autocomplete="off"><br>
+
+        <br>
+        <br>
+
+        <label>Element to delete:</label>
+        <input type="number" name='delete' autocomplete="off"><br>
+
+        <br>
+        <br>
+
+        <label>Number of Elements to be deleted is:</label>
+        <input type="number" name='num_delete' autocomplete="off"><br>
+
+        <br>
+        <br>
+
+
+        <label>Element to search:</label>
+        <input type="number" name='search' autocomplete="off"><br>
+
+        <br>
+        <br>
+
+
+        <input type="submit" name="proceed" value="Proceed" autocomplete="off">
+
+        <br>
+        <br>
+
+        <label>Created Array Is:</label>
+        <?php print_r($created_array);?>
+
+        <br>
+        <br>
+
+
+        <label>Ascending order Array Is:</label>
+        <?php asort($created_array);
+print_r($created_array);?>
+
+        <br>
+        <br>
+
+        <label>Descending order Array Is:</label>
+
+        <?php arsort($created_array);
+print_r($created_array);?>
+
+        <br>
+        <br>
+
+        <label>After Insertion is:</label>
+        <?php print_r($inserted_array);?>
+
+        <br>
+        <br>
+
+        <label>After Deletion is:</label>
+        <?php print_r($deleted_array);?>
+
+        <br>
+        <br>
+
+        <label>Copying of Array:</label>
+        <?php $new_array = $created_array;
+print_r($new_array);?>
+
+        <br>
+        <br>
+
+        <label>Merged Array is:</label>
+        <?php $merged_array = array_merge($created_array, $new_array);
+print_r($merged_array);?>
+
+        <br>
+        <br>
+
+        <label>After Removing 3 elements from an array is:</label>
+        <?php print_r($num_deleted_array);?>
+
+        <br>
+        <br>
+
+
+        <label>Finding the Initial Array Length:</label>
+        <?php echo count($created_array); ?>
+        <br>
+        <br>
+
+        <label>Finding the Merged Array Length :</label>
+        <?php echo count($merged_array); ?>
+
+        <br>
+        <br>
+
+        <label>5 elements inserting,:</label>
+        <?php $created_array['first'] = 'hi';
+$created_array['second'] = 'hello';
+$created_array['third'] = 'how are you?';
+$created_array['fourth'] = 'I am Naveen';
+$created_array['fifth'] = 'bi';
+
+print_r($created_array)?>
+        <br>
+        <br>
+
+        <label>Displaying first 4 elements :</label>
+        <?php $new = $created_array;
+print_r(array_splice($new, 0, 4));?>
+
+        <br>
+        <br>
+
+        <label>Index of the given element:</label>
+        <?php echo $search; ?>
+        <br>
+        <br>
+
+        <label>Unique Element in an Array:</label>
+        <?php print_r(array_unique($created_array));?>
+        <br>
+        <br>
+    </form>
+    <?php
+
+?>
+
+</body>
+
+</html>
